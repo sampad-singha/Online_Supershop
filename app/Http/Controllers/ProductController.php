bf7ef2af6ccff156d16369186f2f775151f9b769
sampad <sampadsingha8@gmail.com>
2025-01-17 14:06:06 +0600

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use http\Exception;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        if($products->count() < 1) {
+            return response()->json(['message' => 'No products found'], 200);
+        }
+        return response()->json($products, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->save();
+
+        return response()->json([
+            'message' => 'Product created successfully',
+            'product' => $product
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->name = $request->name ?? $product->name;
+        $product->price = $request->price ?? $product->price;
+        $product->description = $request->description ?? $product->description;
+        $product->save();
+
+        return response()->json([
+            'message' => 'Product updated successfully',
+            'product' => $product
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully'], 200);
+    }
+}
